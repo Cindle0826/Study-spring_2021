fc9a2ece9baaf8ec9c13f978e632836b6d3e8e1e
cindle <s22603610@yahoo.com.tw>
2021-11-27 18:05:05 +0800

    study spring

---
+++ spring/src/main/java/com/study/spring/case04/PersonDaoImpl.java
@@ -0,0 +1,16 @@
+package com.study.spring.case04;
+
+import java.util.List;
+
+public class PersonDaoImpl implements PersonDao {
+
+	@Override
+	public void add() {
+	}
+
+	@Override
+	public List<Person> queryAll() {
+		return null;
+	}
+
+}
